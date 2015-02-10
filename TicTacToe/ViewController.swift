353d2f0326d119656c5ed60964b014acbb32b86d
SUZUKIyuka <suzukiyuka1218@gmail.com>
2015-02-10 09:37:02 +0900

    add Label and Button

--- TicTacToe/ViewController.swift
+++ TicTacToe/ViewController.swift
@@ -9,6 +9,30 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet var ticTacLabel1 : UILabel!
+    @IBOutlet var ticTacLabel2 : UILabel!
+    @IBOutlet var ticTacLabel3 : UILabel!
+    @IBOutlet var ticTacLabel4 : UILabel!
+    @IBOutlet var ticTacLabel5 : UILabel!
+    @IBOutlet var ticTacLabel6 : UILabel!
+    @IBOutlet var ticTacLabel7 : UILabel!
+    @IBOutlet var ticTacLabel8 : UILabel!
+    @IBOutlet var ticTacLabel9 : UILabel!
+    
+    @IBOutlet var ticTacBtn1 : UIButton!
+    @IBOutlet var ticTacBtn2 : UIButton!
+    @IBOutlet var ticTacBtn3 : UIButton!
+    @IBOutlet var ticTacBtn4 : UIButton!
+    @IBOutlet var ticTacBtn5 : UIButton!
+    @IBOutlet var ticTacBtn6 : UIButton!
+    @IBOutlet var ticTacBtn7 : UIButton!
+    @IBOutlet var ticTacBtn8 : UIButton!
+    @IBOutlet var ticTacBtn9 : UIButton!
+    
+    @IBOutlet var resetBtn : UIButton!
+    
+    @IBOutlet var userMsgLabel : UILabel!
 
     override func viewDidLoad() {
         super.viewDidLoad()
