937b86c6ead24480a8bfe83bc212079ec403b696
SUZUKIyuka <suzukiyuka1218@gmail.com>
2015-02-13 20:41:56 +0900

    add comments

--- TicTacToe/ViewController.swift
+++ TicTacToe/ViewController.swift
@@ -10,6 +10,7 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    //O,Xを表示するラベル 画像でも
     @IBOutlet var ticTacLabel1 : UILabel!
     @IBOutlet var ticTacLabel2 : UILabel!
     @IBOutlet var ticTacLabel3 : UILabel!
@@ -30,27 +31,38 @@ class ViewController: UIViewController {
     @IBOutlet var ticTacBtn8 : UIButton!
     @IBOutlet var ticTacBtn9 : UIButton!
     
+    //ゲーム終了してリセットするボタン
     @IBOutlet var resetBtn : UIButton!
     
+    //どちらが勝ったかor引き分けか表示する
     @IBOutlet var userMsgLabel : UILabel!
     
+    //KeyはO,Xの場所、valueはO,X（Oは0,Xは1）
     var plays = Dictionary<Int,Int>()
+    //ゲームが終わったか
     var done = false
+    //aiを動かすか
     var aiDeciding = false
     
     @IBAction func btClick(sender: UIButton) {
         userMsgLabel.hidden = true
+        //押したところが空欄で、自分の番であったら、そこにXを表示する
         if plays[sender.tag] == nil && aiDeciding == false && done == false {
             setLabelForSpot(sender.tag, player:1)
         }
+        //勝負がついたか判定する
         checkForWin()
+        //aiのターン
         aiTurn()
     }
     
+    //O,XをLabelに表示する
     func setLabelForSpot(spot:Int,player:Int) {
+        //playerが1だったら"X" それ以外は"O"を表示させる
         var playerMark = player == 1 ? "X" : "O"
         plays[spot] = player
         
+        //スポットの番号のlabelにX,Oを表示させる
         switch spot {
         case 1:
             ticTacLabel1.text = playerMark
@@ -72,10 +84,10 @@ class ViewController: UIViewController {
             ticTacLabel9.text = playerMark
         default :
             ticTacLabel5.text = playerMark
-            
         }
     }
     
+    //リセットする
     @IBAction func resetBtClicked(sender: UIButton) {
         done = false
         userMsgLabel.hidden = true
@@ -83,8 +95,9 @@ class ViewController: UIViewController {
         reset()
     }
     
+    //値を初期化する
     func reset() {
-        plays = [:]
+        plays = [:] //初期化
         ticTacLabel1.text = nil
         ticTacLabel2.text = nil
         ticTacLabel3.text = nil
@@ -97,34 +110,41 @@ class ViewController: UIViewController {
         
     }
     
+    //勝負がついたか判定する
     func checkForWin() {
+        //だれが勝ったか
         var whoWon = ["I":0,"you":1]
+        
+        //I(0)の場合とYOU(1)の場合で判定する
         for(key,value) in whoWon {
-            if (plays[7] == value && plays[8] == value && plays[9] == value){
-                
-            }else if (plays[4] == value && plays[5] == value && plays[6] == value) {
-                
-            }else if (plays[1] == value && plays[2] == value && plays[3] == value) {
-                
-            }else if (plays[1] == value && plays[5] == value && plays[9] == value) {
-                
-            }else if (plays[3] == value && plays[5] == value && plays[7] == value) {
-                
-            }else if (plays[1] == value && plays[4] == value && plays[7] == value) {
-                
-            }else if (plays[2] == value && plays[5] == value && plays[8] == value) {
-                
-            }else if (plays[3] == value && plays[6] == value && plays[9] == value){
+            //下の行が揃ったか
+            var isBottomDone = plays[7] == value && plays[8] == value && plays[9] == value
+            //真ん中の行が揃ったか
+            var isMiddlerowDone = plays[4] == value && plays[5] == value && plays[6] == value
+            //上が揃ったか
+            var isTopDone = plays[1] == value && plays[2] == value && plays[3] == value
+            //左から右のななめがそろったか
+            var isLeftRightDone = plays[1] == value && plays[5] == value && plays[9] == value
+            //右から左のななめがそろったか
+            var isRightLeftDone = plays[3] == value && plays[5] == value && plays[7] == value
+            //左が揃ったか
+            var isLeftDone = plays[1] == value && plays[4] == value && plays[7] == value
+            //真ん中の列が揃ったか
+            var isMiddleColumDone = plays[2] == value && plays[5] == value && plays[8] == value
+            //右の列が揃ったか
+            var isRightDone = plays[3] == value && plays[6] == value && plays[9] == value
+            
+            //上のどれかだったらゲーム終了
+            if (isBottomDone || isMiddlerowDone || isTopDone || isLeftRightDone || isRightLeftDone || isLeftDone || isMiddleColumDone || isRightDone) {
+                //ゲーム終了の処理
                 userMsgLabel.hidden = false
                 userMsgLabel.text = "Looks like \(key) won !"
                 resetBtn.hidden = false
                 done = true
-                
             }
-            
         }
-        
     }
+    
     
     func checkBottom(#value:Int) -> (location:String, pattern:String)? {
         return ("bottom",checkFor(value, inList: [7,8,9]))
@@ -180,11 +200,15 @@ class ViewController: UIViewController {
         return Bool(plays[spot]!)
     }
     
+    //AIの動き
     func aiTurn() {
+        //ゲームが終わっていたらなにもしない
         if done {
             return
         }
+        //AI計算中はtrueにする
         aiDeciding = true
+        
         
         if let result = rowCheck(value:0) {
             var whereToPlayResult = whereToPlay(result.location, pattern:result.pattern)
@@ -251,14 +275,14 @@ class ViewController: UIViewController {
         var middlePattern = "101"
         
         switch location {
-            case "top":
-                if pattern == leftPattern {
-                    return 1
-                }else if pattern == rightPattern {
-                    return 3
-                }else {
-                    return 2
-                }
+        case "top":
+            if pattern == leftPattern {
+                return 1
+            }else if pattern == rightPattern {
+                return 3
+            }else {
+                return 2
+            }
         case "bottom":
             if pattern == leftPattern {
                 return 7
@@ -323,12 +347,12 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    
 }
 
